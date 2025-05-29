b2e3cec92a0b92901e39538f24d708dc71abcec6
Monty <montymhango01@gmail.com>
2025-05-29 15:46:54 +0200

    update next.config.mjs
    chnaging code style

--- oddjobs/next.config.mjs
+++ oddjobs/next.config.mjs
@@ -1,17 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      domains: ['qnpwynomeazsbhlenltx.supabase.co'], // Removed https://
-    },
-    webpack: (config) => {
-      config.externals.push({
-        'utf-8-validate': 'commonjs utf-8-validate',
-        bufferutil: 'commonjs bufferutil',
-      });
-      return config;
-    },
-  };
-  
-  export default nextConfig;
-  
+  reactStrictMode: true,
+  images: {
+    domains: ['qnpwynomeazsbhlenltx.supabase.co'],
+  },
+  webpack: (config) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      bufferutil: 'commonjs bufferutil',
+    });
+    return config;
+  },
+};
+
+module.exports = nextConfig;  // Changed from export default
