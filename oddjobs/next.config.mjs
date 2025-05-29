44c1dde6b8b2ebbb59ebc539ea79666f5d77c12c
Monty <montymhango01@gmail.com>
2025-05-29 16:00:16 +0200

    created next.config.js
    created the .mjs file

---
+++ oddjobs/next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: ['qnpwynomeazsbhlenltx.supabase.co'],
+    },
+    webpack: (config) => {
+      config.externals.push({
+        'utf-8-validate': 'commonjs utf-8-validate',
+        'bufferutil': 'commonjs bufferutil',
+      });
+      return config;
+    },
+  };
+  
+  export default nextConfig;  // ✅ ES Modules syntax
