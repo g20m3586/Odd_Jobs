b60158e02a524c22bd769e0ade5e7a2f9fe9c0a7
Monty <61051883+g20m3586@users.noreply.github.com>
2025-07-07 14:53:13 +0200

    Add avatar image list and update image config
    Introduced a new avatars.js module exporting a list of avatar image URLs from i.pravatar.cc. Updated next.config.js to allow remote images from i.pravatar.cc using remotePatterns.

---
+++ oddjobs/src/lib/avatars.js
@@ -0,0 +1,15 @@
+// src/lib/avatars.js
+const avatars = [
+  "https://i.pravatar.cc/100?img=1",
+  "https://i.pravatar.cc/100?img=2",
+  "https://i.pravatar.cc/100?img=3",
+  "https://i.pravatar.cc/100?img=4",
+  "https://i.pravatar.cc/100?img=5",
+  "https://i.pravatar.cc/100?img=6",
+  "https://i.pravatar.cc/100?img=7",
+  "https://i.pravatar.cc/100?img=8",
+  "https://i.pravatar.cc/100?img=9",
+  "https://i.pravatar.cc/100?img=10",
+]
+
+export default avatars
